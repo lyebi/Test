00593ccd9157b33f2cb8c575d25a2a81e1147d21
liboxing <964207634@qq.com>
2018-07-23 11:07:07 +0800

    test

--- a3c.py
+++ a3c.py
@@ -452,6 +452,7 @@ should be computed.
             # reward = min(1, max(-1, reward))
             if reward>0:
                 reward/=100.0
+                self.beta=self.beta+(1-self.beta)*0.0001
             else:
                 reward = min(1, max(-1, reward))
 
